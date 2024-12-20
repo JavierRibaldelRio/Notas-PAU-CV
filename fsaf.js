f81e86e873b32fd7a7f1d26bdf834f53354908d6
Javier Ribal del Río <javierribal@gmail.com>
2024-12-20 17:48:07 +0100

    typo: change folder name

---
+++ fsaf.js
@@ -0,0 +1,22 @@
+const universo = {
+    sistema_solar: {
+        terra: {
+            europa: {
+                espanya: {
+
+
+
+
+                    onti: [["Pau", "Mami"]],
+                    valen: [["Javier Ri", "Pablo"], ["Pau", "&CIA"]],
+                    san_salvador: [["Javier", "Pablo"]]
+
+
+
+                }
+            }
+        }
+    }
+}
+
+console.log(universo.sistema_solar.terra.europa.espanya.valen[1][0]);
