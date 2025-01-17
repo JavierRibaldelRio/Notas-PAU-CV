1f74e13c13989ca57fdf8f16209fde72999ab7b3
Javier Ribal del Río <javierribal@gmail.com>
2025-01-17 18:26:57 +0100

    fix: change file name, move data transformation to aside script

--- utils/data_extraction_pdf/extract_marks.py
+++ utils/data_extraction_pdf/extract_marks.py
@@ -13,6 +13,9 @@ def extract_marks_from_pdf(convo, year):
         # For every page in the pdf
         for page in pdf.pages:
 
+            # Get pages of modality subject
+            extracted_text = page.extract_text()
+
             # ? Possible optimització del for, una vegada ja tingas la primera pàgina on s'hi troben les asignatures de modalitat ja no cal fer més iterracions car si hi són dos pàgines amb es la següent, es a dir; una vegada tens la primera pàgina  solaments tens que mirar si la següent té la paraula UA
 
             # Extraction of the table of obligatory subjects
@@ -33,8 +36,6 @@ def extract_marks_from_pdf(convo, year):
                 table.extend(extracted_table_special)
                 continue
 
-            # Get pages of modality subject
-            extracted_text = page.extract_text()
             if (
                 "SUV" in extracted_text
                 and "Resultats globals per assignatura" in extracted_text
@@ -68,27 +69,6 @@ def extract_marks_from_pdf(convo, year):
                 break
 
             table.extend(extracted_table)
-
-    # TODO: Aquesta part de transformació la moverem a altre puesto per a profitar y fer la transformació de llista a tupla, per a facilitar la inserció a la base de dades
-
-    # Converts all the numeric elements that where interpreted as strings to float (or int if has no decimals)
-    for fila in table:
-
-        # ? Se
-        # Adds a new column to the table with the number of people that passed the obligatory phase
-        fila.append(int(fila[3]) - int(fila[9]))
-
-        for i in range(1, len(fila)):
-            if isinstance(fila[i], str):
-                fila[i] = fila[i].replace(",", ".")
-                if not fila[i].find(".") == -1:
-                    fila[i] = float(fila[i])
-                else:
-                    fila[i] = int(fila[i])
-
-        # Adds two more columns to the table, with the year of the convocatory, and which convocatori
-        fila.append(year)
-        fila.append(convo)
 
     # final order of the rows: subject, ... (like in the pdf), pass_obligatory_phase, year, convo.
     return table
