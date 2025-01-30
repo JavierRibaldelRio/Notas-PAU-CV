1865f0d7d8111c205bb2ba14fd81654a13014def
Javier Ribal del Río <javierribal@gmail.com>
2025-01-30 20:10:52 +0100

    feat: add provinces

---
+++ utils/data_extraction_pdf/database_utils/database_generation/create-tables-provinces-regions-municipalities.sql
@@ -0,0 +1,32 @@
+# The tables of "regions", "provinces" y "municipalities"tienen referencias entre sí, por lo que hay que crearlas con el siguiente script
+
+CREATE TABLE IF NOT EXISTS municipalities(
+    id INTERGER PRIMARY KEY,
+    ine_code TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL UNIQUE,
+    region INTEGER,
+
+    FOREIGN KEY(region) REFERENCES regions(id)
+);
+
+CREATE TABLE IF NOT EXISTS regions(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    region_head INTEGER,
+    province INTEGER,
+
+    FOREIGN KEY (region_head) REFERENCES municipalities(id),
+    FOREIGN KEY (province) REFERENCES provinces(id)
+
+);
+
+CREATE TABLE IF NOT EXISTS provinces(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    provincial_capital INTEGER,
+
+    FOREIGN KEY (provincial_capital) REFERENCES municipalities(id)
+);
+
+# Mofify municipalities to add the missing FOREIGN KEY
+
