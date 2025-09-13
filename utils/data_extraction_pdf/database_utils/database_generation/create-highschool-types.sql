ee520c6774d095351935bfc1679e3af0c801a01f
Javier Ribal del Río <javierribal@gmail.com>
2025-09-13 16:12:53 +0200

    feat: remove old sql files

--- utils/data_extraction_pdf/database_utils/database_generation/create-highschool-types.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS high_school_types(
-    id INTEGER PRIMARY KEY,
-    type TEXT NOT NULL UNIQUE
-);
-
-INSERT INTO high_school_types (id, type) VALUES
-    (0, 'público'),
-    (1, 'privado-concertado'),
-    (2, 'privado')
