d7408adf9d90d789c96fb72ebc276406e455898f
Javier Ribal del Río <javierribal@gmail.com>
2025-09-14 21:00:42 +0200

    WIP:  main structure

---
+++ app.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(bslib)
+
+ui <- page_navbar(
+  title = "Análisis Notas PAU",
+  id = "page",
+  theme = bs_theme(preset = "litera"),
+
+  navbar_options = navbar_options(
+    class = "bg-primary",
+    theme = c("auto", "light", "dark"),
+    underline = TRUE
+  ),
+
+  # Page 1
+  nav_panel("Asignaturas", "Page A content"),
+
+  nav_menu(
+    "Regiones",
+    nav_panel(
+      "Provincia",
+      "1"
+    ),
+    nav_panel(
+      "Comarca",
+      "ASDF"
+    ),
+    nav_panel(
+      "Municipio",
+      "ASDF"
+    )
+  ),
+
+  nav_menu(
+    "Centros",
+    nav_panel(
+      "Centros",
+      "1"
+    ),
+    nav_panel(
+      "Buscador",
+      "ASDF"
+    ),
+  ),
+
+  nav_panel("Sobre nosotros", "Pau y Javier"),
+
+  nav_item(input_dark_mode(id = "mode")),
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
