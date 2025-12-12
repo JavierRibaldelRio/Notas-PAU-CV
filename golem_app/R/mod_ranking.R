8dee9fba1de0755ffaeb1adf7c339a67a1680744
Pau Minguet <pminmic@gmail.com>
2025-12-12 20:41:29 +0100

    feat: ranking table

--- golem_app/R/mod_ranking.R
+++ golem_app/R/mod_ranking.R
@@ -10,235 +10,257 @@
 mod_ranking_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    
-    # Solución con CSS para que la cabezera de la tabla con dos celdas fusionadas esté centrada
+    # CSS for the table
     tags$head(
       tags$style(HTML(
-        # #ns("miDataTable"), busca la tabla con el ID de tu módulo
-        # thead tr:first-child, busca la PRIMERA fila del encabezado
-        # th, aplica el estilo a todas las celdas de esa fila
         paste0(
-          "#", ns("ranking_subjects_dt"), " thead tr:first-child th { 
-            text-align: center; 
+          "#", ns("ranking_subjects_dt"), " table.dataTable { font-size: 16px !important; border: 2px solid #555 !important;}",
+
+          "#", ns("ranking_subjects_dt"), " table.dataTable td, ",
+          "#", ns("ranking_subjects_dt"), " table.dataTable th { ",
+          "   vertical-align: middle !important;",
+          "}",
+
+          "#", ns("ranking_subjects_dt"), " thead tr:first-child th { text-align: center; }",
+          
+          "#", ns("ranking_subjects_dt"), " thead tr:first-child th, ",
+          "#", ns("ranking_subjects_dt"), " thead tr:nth-child(2) th:nth-child(2n) ,",
+          "#", ns("ranking_subjects_dt"), " tbody td:nth-child(2n+1) { 
+              border-right: 2px solid #999 !important; 
           }"
         )
       ))
     ),
 
+    br(), # Separation between the top of the card and selector
+
     fluidRow(
+      # Centered container
       column(
-        width = 4,
-        # Radio buttons para elegir el método que usaremos
-        radioButtons(
-          inputId = ns("ranking_subjects"),
-          label = "Elige una opción: ",
-          inline = TRUE,
-          choices = list(
-            "Global", 
-            "Año" 
-          ),
-          selected = "Global"
-        )
-      ),
-      column(
-        width = 4,
-        # Si se selecciona año, entonces
-        conditionalPanel(
-          ns = ns,
-          condition = "input.ranking_subjects == 'Año'",
-          selectInput( 
-            ns("ranking_subjects_select"), 
-            "Select options below:", 
-            list(
-              "2010" = 2010, 
-              "2011" = 2011, 
-              "2012" = 2012,
-              "2013" = 2013,
-              "2014" = 2014,
-              "2015" = 2015,
-              "2016" = 2016,
-              "2017" = 2017,
-              "2018" = 2018,
-              "2019" = 2019,
-              "2020" = 2020,
-              "2021" = 2021,
-              "2022" = 2022,
-              "2023" = 2023,
-              "2024" = 2024
-            ) 
-          ), 
-        ),
-      )
-    ),
-    DTOutput(ns("ranking_subjects_dt"))
-    
-  )
-}
-
-# Estructura de la tabla
-create_table_structure <- function(input, output, session, pool) {
-
-  output$ranking_subjects_dt <- renderDT({
-    
-   # Creamos el esquema del encabezado
-    sketch <- withTags(table(
-      class = 'display',
-      thead(
-        # Fila 1 del header: aquí fusionamos
-        tr(
-          th(colspan = 2, 'Mejores Notas'), # Fusiona 2 columnas
-          th(colspan = 2, 'Peores Notas'),
-          th(colspan = 2, 'Mayor Presentados'),
-          th(colspan = 2, 'Menor Presentados')
-        ),
-        # Fila 2 del header: los nombres reales
-        tr(
-          th('Asignatura'),
-          th('Notas'),
-          th('Asignatura'),
-          th('Notas'),
-          th('Asignatura'),
-          th('Presentados'),
-          th('Asignatura'),
-          th('Presentados')
-        )
-      )
-    ))
-    # 2. Datos
-
-    dt_calculado <- reactive({
-
-      req(input$mi_filtro_o_boton)
-  
-      # 1. Llama a tu función, que DEBE devolver un data frame
-      # (Asumo que esta función USA inputs, p.ej. input$mi_filtro)
-      datos_brutos <- ranking_selection(input, output, session, pool) 
-
-      # 2. Transfórmalo
-      transform_to_table(datos_brutos)
-
-      # El data frame transformado es lo que "devuelve" este bloque reactivo
-    })
-
-    # 3. Renderizar la tabla
-    datatable(
-      dt_calculado,
-      container = sketch, # Usamos el encabezado personalizado
-      rownames = FALSE,
-      options = list(
-        dom = 't',
-        ordering = FALSE,
-        columnDefs = list(
-          list(
-            className = 'dt-head-center',
-            targets = '_all'
+        width = 8, 
+        offset = 2,
+        
+        # This creates the gray background
+        wellPanel(
+          fluidRow(
+            # Radio buttons
+            column(
+              width = 6,
+              align = "center",
+              radioButtons(
+                inputId = ns("ranking_subjects"),
+                label = "Modo de visualización",
+                choices = list("Global", "Año"),
+                selected = "Global",
+                inline = TRUE
+              )
+            ),
+            
+            # Selector (only appears if "Año" is selected)
+            column(
+              width = 6,
+              align = "center",
+              conditionalPanel(
+                ns = ns,
+                condition = "input.ranking_subjects == 'Año'",
+                selectInput( 
+                  ns("ranking_subjects_select"), 
+                  "Selecciona el año",
+                  choices = 2010:2024,
+                  width = "100%" 
+                )
+              )
+            )
           )
         )
       )
+    ),
+    # Here we show the table
+    fluidRow(
+      # Centered table
+      column(
+        width = 10,
+        offset = 1,
+        DTOutput(ns("ranking_subjects_dt"))
+      )
     )
-  })
+  )
 }
 
+# Transform data
 transform_to_table <- function(dt) {
   umbral_presentados <- 30
 
-  # Hacemos la mediana para la global, claro si es de un año específico la media de cada uno será lo mismo
+  # Mean
   notas <- dt |> 
-    group_by(code) |> 
-    summarize(
+    group_by(name) |> 
+    summarise(
       average = mean(average, na.rm = TRUE),
       candidates = mean(candidates, na.rm = TRUE)
     ) |> 
     ungroup()
 
+  # Filter by grades
   notas_filtradas <- notas |> 
     filter(candidates > umbral_presentados)
 
-  # IMPORTANTE: Renombramos columnas para que bind_cols no cree duplicados
-  ranking_mejores_notas <- notas_filtradas |> 
-    arrange(desc(average)) |> 
-    select(
-      Asignatura_Mejor = code, # Renombrado
-      Notas_Mejor = average
+  
+  # With the purpose of having the number of the ranking as another col
+  best_grades <- notas_filtradas |> 
+      arrange(desc(average)) |> 
+      select(
+        Asig_Mejor = name, 
+        Nota_Mejor = average
     )
   
-  ranking_peores_notas <- notas_filtradas |> 
+  # Binding of all data into one
+  ranking_final <- bind_cols(
+  # Thanks to best_grades we can have the number in the ranking
+  tibble(Pos = 1:nrow(best_grades)),
+  
+  # Best grades
+  best_grades,
+
+  # Worst grades
+  notas_filtradas |> 
     arrange(average) |> 
     select(
-      Asignatura_Peor = code, # Renombrado
-      Notas_Peor = average
-    )
-  
-  ranking_mas_presentados <- notas_filtradas |> 
+      Asig_Peor = name, 
+      Nota_Peor = average
+    ),
+
+  # More candidates
+  notas_filtradas |> 
     arrange(desc(candidates)) |> 
     select(
-      Asignatura_Mas = code, # Renombrado
-      Presentados_Mas = candidates
-    )
-  
-  ranking_menos_presentados <- notas_filtradas |> 
+      Asig_Mas = name, 
+      Cand_Mas = candidates
+    ),
+
+  # Less candidates
+  notas_filtradas |> 
     arrange(candidates) |> 
     select(
-      Asignatura_Menos = code, # Renombrado
-      Presentados_Menos = candidates
+      Asig_Menos = name, 
+      Cand_Menos = candidates
     )
-  
-  ranking_final <- bind_cols(
-    ranking_mejores_notas,
-    ranking_peores_notas,
-    ranking_mas_presentados,
-    ranking_menos_presentados
   )
 
-  return (ranking_final)
+  return(ranking_final)
 }
     
 #' ranking Server Functions
 #'
 #' @noRd 
-mod_ranking_server <- function(id,pool){
+mod_ranking_server <- function(id, pool) {
 
-  moduleServer(id, function(input, output, session){
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
-    datos_brutos <- reactive ({
 
+    # Data extraction from DB
+    datos_brutos <- reactive({
       req(input$ranking_subjects)
       opcion <- input$ranking_subjects
 
+      # Different query for different the selected table
       if (opcion == "Global") {
         df <- dbGetQuery(
           pool,
-          "SELECT subjects.code, marks.average, marks.candidates FROM marks INNER JOIN subjects ON subjects.id = marks.subject_id;"
+          "SELECT subjects.name, marks.average, marks.candidates FROM marks INNER JOIN subjects ON subjects.id = marks.subject_id;"
         )
         return(df)
         
-      } else {
+      } else if (opcion == "Año"){
         req(input$ranking_subjects_select)
-        year <- imput$ranking_subjects_select
+        year <- input$ranking_subjects_select 
 
         sql_query <- glue_sql(
-          "SELECT subjects.code, marks.average, marks.candidates FROM marks INNER JOIN subjects on subjects.id = marks.subjects_id WHERE marks.year {`year`};",
+          "SELECT subjects.name, marks.average, marks.candidates FROM marks INNER JOIN subjects on subjects.id = marks.subject_id WHERE marks.year = {year};",
           .con = pool
         )
 
-        df <- dbGetQuery(
-          pool, 
-          sql_query
-        )
-
+        df <- dbGetQuery(pool, sql_query)
         return(df)
       }
     })
-    # Este output reacciona a los cambios en el radio button
-    # output$resultado <- input$ranking_subjects
-    ranking_selection(input, output, session, pool)
-    create_table_structure(input, output, session, pool)
+
+    # Data transform
+    datos_finales <- reactive({
+      req(datos_brutos()) # Wait until the data is ready
+      transform_to_table(datos_brutos())
+    })
+
+    # Render the table
+    output$ranking_subjects_dt <- renderDT({
+      
+      req(datos_finales()) # Wait until the table is ready
+
+      # Custom header
+      sketch <- withTags(table(
+        class = 'display cell-border compact',
+        thead(
+          tr(
+            th(rowspan = 2, '#'),
+            th(colspan = 2, 'Mejores calificaciones'),
+            th(colspan = 2, 'Peores calificaciones'),
+            th(colspan = 2, 'Mayor número de presentados'),
+            th(colspan = 2, 'Menor número de presentados')
+          ),
+          tr(
+            th('Asignatura'), th('Nota'),
+            th('Asignatura'), th('Nota'),
+            th('Asignatura'), th('Presentados'),
+            th('Asignatura'), th('Presentados')
+          )
+        )
+      ))
+
+      datatable(
+        datos_finales(), 
+        container = sketch,
+        rownames = FALSE,
+        options = list(
+          dom = 't',
+          ordering = FALSE, # Is already ordered
+          autoWidth = FALSE,
+          scrollColapse = TRUE,
+          columnDefs = list(
+            # Ranking position
+            list(
+              targets = 0,
+              className = 'dt-center dt-bold', 
+              width = '4%',                    
+              searchable = FALSE
+            ),
+            # Subject names
+            list(
+              targets = c(1, 3, 5, 7),
+              className = 'dt-left',
+              width = '18%'          
+            ),
+            
+            # Grades
+            list(
+              targets = c(2, 4, 6, 8),
+              className = 'dt-center', 
+              width = '7%'             
+            )
+          )
+        )
+      ) |> 
+        # Change ',' by '.' and vice versa, and specific decimal digits
+        formatRound(
+          columns = c("Nota_Mejor", "Nota_Peor"), 
+          digits = 3,
+          dec.mark = ",",
+          mark = "."
+        ) |> 
+        formatRound(
+          columns = c("Cand_Mas", "Cand_Menos"), 
+          digits = 2,
+          dec.mark = ",",
+          mark = "."
+        )
+    })
   })
 }
-    
-## To be copied in the UI
-# mod_ranking_ui("ranking_1")
-    
-## To be copied in the server
-# mod_ranking_server("ranking_1")
-  
